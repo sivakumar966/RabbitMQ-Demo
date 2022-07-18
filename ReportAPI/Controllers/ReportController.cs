fdc6818bf2b330c4f481c0c286876c554f780fd9
sivakumar966 <sivakumar966@gmail.com>
2022-07-18 16:18:16 +0530

    Report controller Added

---
+++ ReportAPI/Controllers/ReportController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReportAPI.Data;
+
+namespace ReportAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+
+        public ReportController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet("products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await dbContext.Products.Select(x => new { x.ProductId, x.ProductName, x.Price, x.Ordered, x.Stock }).ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("orders")]
+        public async Task<IActionResult> GetOrders()
+        {
+            var orders = await dbContext.Orders.Select(x => new { x.OrderId, x.Product, x.Quantity, x.Username }).ToListAsync();
+            return Ok(orders);
+        }
+
+    }
+}
